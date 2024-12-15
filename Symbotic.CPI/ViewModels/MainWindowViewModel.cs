862d25c52d6ac501a3f86f22eeff088b8dcd4953
Steve McKean <steven.mckean@peraton.com>
2024-12-15 06:39:39 -0600

    Main window change. build.yml changed.

--- Symbotic.CPI/ViewModels/MainWindowViewModel.cs
+++ Symbotic.CPI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,9 @@
-﻿namespace Symbotic.CPI.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Symbotic.CPI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public string Greeting { get; set; } = "Welcome to Avalonia!";
+
 }
